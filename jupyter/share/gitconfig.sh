316c8b12d6be75cbeb3c785c9f4d7fb8a2726b81
Joseph Li <jli@coursera.org>
2021-06-09 15:39:19 -0700

    git persistence and default options jupyter

---
+++ jupyter/share/gitconfig.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/jovyan/work/.dotfiles-coursera
+# ln -s /home/jovyan/work/.dotfiles-coursera/.gitconfig /home/jovyan/.gitconfig
+# ln -s /home/jovyan/work/.dotfiles-coursera/.git-credentials /home/jovyan/.git-credentials
+git config --global core.fileMode false
