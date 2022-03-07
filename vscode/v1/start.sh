a7c9aba96a7de976e1c39077f791fb21f0b8a464
Joseph Li <jli@coursera.org>
2022-03-07 11:05:10 -0800

    sync vscode files

--- vscode/v1/start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python3 ${VSCODE_USER}/coursera/submitButton.py
-code-server --config /tmp/config/code-server/config.yaml
--- vscode/v2/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python3 ${VSCODE_USER}/coursera/submitButton.py
-mkdir -p /home/coder/project/.dotfiles-coursera
-git config --global core.fileMode false
-code-server --config /tmp/config/code-server/config.yaml
