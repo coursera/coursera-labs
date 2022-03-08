aa5e0ecaad4bd46b7dfc1f8a186c4ed39976a92b
Joseph Li <jli@coursera.org>
2022-03-08 10:58:52 -0800

    generate feedback file in nbgrader image

--- jupyter/nbgrader/grader/scrub.py
+++ jupyter/nbgrader/grader/scrub.py
@@ -1,6 +1,8 @@
 import os,re,json,sys
+import shutil
 
 # Process all the files
 if __name__ == "__main__":
 	print("Redaction functionality is removed for coursera-labs image for academic integrity purposes.")
+	shutil.copyfile(sys.argv[1], sys.argv[1]+'.clean')
 
