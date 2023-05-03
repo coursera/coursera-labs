7c5550baaa2d8cd7f5aa93341a652e4ef5a3410f
Joseph Li <jli@coursera.org>
2023-05-02 17:01:08 -0700

    Update Linux Desktop to release state (#42)

---
+++ linux-desktop/coursera-start.sh
@@ -0,0 +1,22 @@
+# Install font
+mkdir -p /home/coder/.local/share/fonts/googlefonts
+unzip -d /home/coder/.local/share/fonts/googlefonts/ /tmp/Source_Sans_Pro.zip &> /dev/null
+fc-cache -fv &> /dev/null
+
+# For persistence
+mkdir -p /home/coder/coursera/.coursera-dotfiles/
+
+# Persist IntelliJ data collection setting
+mkdir -p /home/coder/.local/share/JetBrains/consentOptions
+touch /home/coder/coursera/.coursera-dotfiles/accepted
+ln -s /home/coder/coursera/.coursera-dotfiles/accepted /home/coder/.local/share/JetBrains/consentOptions/
+
+# Persist IntelliJ user agreement setting
+mkdir -p /home/coder/.java/
+mkdir -p /home/coder/coursera/.coursera-dotfiles/.userPrefs/jetbrains/'_!(!!cg"p!(}!}@"j!(k!|w"w!'\''8!b!"p!'\'':!e@=='
+ln -s /home/coder/coursera/.coursera-dotfiles/.userPrefs/ /home/coder/.java/.userPrefs
+
+# Persist user config (recent projects, trust project, etc.)
+mkdir -p /home/coder/coursera/.coursera-dotfiles/.config/JetBrains
+ln -s /home/coder/coursera/.coursera-dotfiles/.config/JetBrains /home/coder/.config/JetBrains
+
---
+++ linux-desktop/syspref.js
@@ -0,0 +1,2 @@
+pref("browser.startup.homepage", "/tmp/firefox-launch-page/index.html");
+pref("startup.homepage_welcome_url", "/tmp/firefox-launch-page/index.html");
